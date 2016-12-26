2a3509be814c5698b79c638b9f0227469c129465
steve <Steve@Macintosh.PK5001Z>
2016-12-26 11:43:04 -0800

    Tweaks

--- js/script.js
+++ js/script.js
@@ -24,12 +24,12 @@ $("#blog").hide();
     $("#winebutton").click(function(){
 
       if( $( "#winelist" ).is(":visible")) {
-        $("#winelist").slideUp(800);
+        $("#winelist").fadeOut(1000).slideUp(800);
         $("#winebutton").removeAttr("style");
       }
       else{
-        $( "#winelist" ).slideToggle( 1000);
-        $("#winebutton").css("box-shadow"," 0px 0px 30px #eee").css("border-radius","20px");
+        $( "#winelist" ).delay( 800 ).slideToggle( 1000);
+        $("#winebutton").css("box-shadow"," 0px 0px 20px #c6ddf0").css("border-radius","20px");
       }
 
       if( $( "#winemaking" ).is(":visible")) {
@@ -56,7 +56,7 @@ $("#blog").hide();
       }
       else{
         $( "#winemaking" ).slideToggle( 1000);
-        $("#makingbutton").css("box-shadow"," 0px 0px 30px #eee").css("border-radius","20px");
+        $("#makingbutton").css("box-shadow"," 0px 0px 20px #c6ddf0").css("border-radius","20px");
       }
 
       if( $( "#winelist" ).is(":visible")) {
@@ -83,7 +83,7 @@ $("#blog").hide();
       }
       else{
         $( "#projects" ).slideToggle( 1000);
-        $("#projectsbutton").css("box-shadow"," 0px 0px 30px #eee").css("border-radius","20px");
+        $("#projectsbutton").css("box-shadow"," 0px 0px 20px #c6ddf0").css("border-radius","20px");
       }
 
       if( $( "#winelist" ).is(":visible")) {
@@ -109,7 +109,7 @@ $("#blog").hide();
       }
       else{
         $( "#blog" ).slideToggle( 1000);
-        $("#blogbutton").css("box-shadow"," 0px 0px 30px #eee").css("border-radius","20px");
+        $("#blogbutton").css("box-shadow"," 0px 0px 20px #c6ddf0").css("border-radius","20px");
       }
 
       if( $( "#winelist" ).is(":visible")) {
